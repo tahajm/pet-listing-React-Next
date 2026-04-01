2ca3ebd40b3600e371e9533906702528ad6f9025
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-01 20:59:13 +0200

    chore: reorder exports in index.ts to include FilterBar and PetList components for better organization

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
-export * from './FilterBar';
 export * from './Card';
 export * from './Container';
+export * from './FilterBar';
 export * from './Footer';
 export * from './Header';
+export * from './PetList';
