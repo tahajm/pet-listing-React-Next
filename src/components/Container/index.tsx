d4cb0091a58c1a9c58c790ee092e80dafbc3efbe
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-01 11:50:18 +0200

    fix: add TypeScript type annotation for children prop in Container component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,5 +1,5 @@
 import styles from './index.module.css';
 
-export const Container = ({ children }) => {
+export const Container = ({ children }: { children: React.ReactNode }) => {
   return <div className={styles.container}>{children}</div>;
 };
