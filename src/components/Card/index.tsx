e95b93e74caa5dcafc6361fa4198a939f1b5788c
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-01 11:35:44 +0200

    feat: enhance Card component with accessibility an use next image

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,15 +1,31 @@
+import Image from 'next/image';
 import styles from './index.module.css';
 
-export function Card({ name, image }: { name: string; image: string }) {
+export function Card({
+  name,
+  image,
+  priority = false,
+}: {
+  name: string;
+  image: string;
+  priority?: boolean;
+}) {
   return (
-    <div className={styles.container}>
+    <li className={styles.container}>
       <div className={styles.imageContainer}>
-        <img src={image} alt={name} className={styles.image} />
+        <Image
+          src={image}
+          alt={`Photo of ${name}`}
+          className={styles.image}
+          fill
+          sizes="(min-width: 768px) 33vw, 150px"
+          priority={priority}
+        />
       </div>
       <h3 className={`${styles.petName} h4`}>{name}</h3>
-      <a href="#petDetail" className={styles.cta}>
+      <a href="#petDetail" className={styles.cta} aria-label={`View ${name}`}>
         View
       </a>
-    </div>
+    </li>
   );
 }
