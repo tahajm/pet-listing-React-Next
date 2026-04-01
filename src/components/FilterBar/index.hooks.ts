67f851f0c7437a71e86d98e5ae3c2c8c00e929a7
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-01 11:39:13 +0200

    feat: implement FilterBar component for filtering pets by species and sorting options

---
+++ src/components/FilterBar/index.hooks.ts
@@ -0,0 +1,36 @@
+import { ChangeEvent } from 'react';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { updateQueryString } from '@/lib/util';
+
+export function useFilterBar() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  const sortBy = searchParams.get('sortBy');
+  const order = searchParams.get('order');
+  const species = searchParams.get('species');
+
+  const isLatestEnabled = sortBy === 'dateAdded' && order === 'desc';
+  const selectedSpecies = species ?? '';
+
+  const toggleLatestEnabled = () => {
+    const newQueries = isLatestEnabled
+      ? { sortBy: '', order: '' }
+      : { sortBy: 'dateAdded', order: 'desc' };
+    const queryString = updateQueryString(searchParams, newQueries);
+    router.push(`${pathname}?${queryString}`);
+  };
+
+  const handleChangeSpecies = (e: ChangeEvent<HTMLSelectElement>) => {
+    const queryString = updateQueryString(searchParams, { species: e.target.value });
+    router.push(`${pathname}?${queryString}`);
+  };
+
+  return {
+    toggleLatestEnabled,
+    handleChangeSpecies,
+    selectedSpecies,
+    isLatestEnabled,
+  };
+}
---
+++ src/components/FilterBar/index.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import styles from './index.module.css';
+import { useFilterBar } from './index.hooks';
+
+export function FilterBar({ species }: Readonly<{ species: string[] }>) {
+  const { handleChangeSpecies, toggleLatestEnabled, isLatestEnabled, selectedSpecies } =
+    useFilterBar();
+
+  return (
+    <form role="search" aria-label="Filter pets" className={styles.form}>
+      <fieldset className={styles.filterBar}>
+        <legend className="srOnly">Filter pets</legend>
+        <div className={styles.selectWrapper}>
+          <label htmlFor="pet-species-select" className="srOnly">
+            Filter by species
+          </label>
+          <select
+            name="petSpecies"
+            id="pet-species-select"
+            value={selectedSpecies}
+            onChange={handleChangeSpecies}
+            className={styles.select}
+          >
+            <option value="">Species</option>
+            {species.map(speciesItem => (
+              <option value={speciesItem} key={speciesItem}>
+                {speciesItem}
+              </option>
+            ))}
+          </select>
+          <img src="/icons/chevron-down.svg" alt="" aria-hidden="true" className={styles.chevron} />
+        </div>
+        <button
+          onClick={toggleLatestEnabled}
+          className={styles.toggle}
+          aria-pressed={isLatestEnabled}
+        >
+          Latest added
+        </button>
+      </fieldset>
+    </form>
+  );
+}
