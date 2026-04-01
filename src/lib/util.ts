ba5d21a7a717cad22b02e5d68e98d2879c61e17d
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-01 11:42:26 +0200

    feat: implement utility functions for query string manipulation

---
+++ src/lib/util.ts
@@ -0,0 +1,21 @@
+import { SearchParams } from '@/types';
+
+export function toQueryString(input: SearchParams) {
+  const params = new URLSearchParams();
+
+  Object.entries(input).forEach(([key, value]) => {
+    if (!value) return;
+
+    if (Array.isArray(value)) {
+      value.forEach(v => params.append(key, v));
+    } else {
+      params.set(key, value);
+    }
+  });
+
+  return params.toString();
+}
+
+export function updateQueryString(current: URLSearchParams, updates: SearchParams): string {
+  return toQueryString({ ...Object.fromEntries(current.entries()), ...updates });
+}
