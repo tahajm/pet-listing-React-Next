e67a0fde7d57bb69f8e4b80f788627b792e876fc
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-08-01 13:56:33 +0200

    chore: cleanup the name

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src="/icons/pet-icon.svg" alt="Coolblue Pets Logo" />
+      <img src="/icons/pet-icon.svg" alt="Pets Logo" />
     </header>
   );
 }
